8a181c930df0fec5678d7ec1567854377f8a8e8f
spidy-00-5 <gowtham398gs@gmail.com>
2025-12-25 16:06:25 +0530

    home page completed

--- app/src/main/java/com/example/celler_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/celler_app/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.celler_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 val GreenPrimary = Color(0xFF2ECC71)
-val GreenDark = Color(0xFF27AE90)
+val GreenDark = Color(0xFF1B5E20)
 val GreenLight = Color(0xFFDFF9EA)
 val BackgroundLight = Color(0xFFF8FFFB)
 val TextPrimary = Color(0xFF1E1E1E)
--- app/src/main/java/com/example/celler_app/uiscreen/Home.kt
+++ app/src/main/java/com/example/celler_app/uiscreen/Home.kt
@@ -1,62 +1,107 @@
 package com.example.celler_app.uiscreen
 
-import android.util.Log.i
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.celler_app.R
-import com.example.celler_app.ui.theme.GreenPrimary
+import com.example.celler_app.ui.theme.CellerAppTheme
 
-
-
-
+//this page contains ui of home screen of the app ,
+// it has two buttons one for fake call and other for emergency mode
 @Composable
 fun HomeScreen(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     onFakeCall: () -> Unit,
     onEmergency: () -> Unit,
-){
+) {
     Column(
-        modifier = Modifier
-            .padding(10.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            modifier = Modifier.padding(10.dp),
-            text = stringResource(R.string.greeting),
-            style = MaterialTheme.typography.headlineLarge.copy(color = Color.Black)
-        )
-        TextButton(
-            onClick = onFakeCall,
-            modifier = Modifier.padding(10.dp),
-            shape = RoundedCornerShape(16.dp),
-        ){
+        modifier = Modifier.fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
-                text = stringResource(R.string.fakecall),
-                style = MaterialTheme.typography.labelLarge
+                modifier = Modifier.padding(bottom = 24.dp),
+                text = stringResource(R.string.greeting),
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+            Button(
+                onClick = onFakeCall,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(14.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary
+                )
+            ) {
+                Text(
+                    text = stringResource(R.string.fakecall),
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+            Text(
+                modifier = Modifier.padding(top = 5.dp, bottom = 24.dp),
+                text = stringResource(R.string.fakecall_desc),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f)
+            )
+
+            Button(
+                onClick = onEmergency,
+                modifier = Modifier
+                    .fillMaxWidth(),
+
+                shape = RoundedCornerShape(14.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary
+                )
+            ) {
+                Text(
+                    text = stringResource(R.string.emergency),
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+            Text(
+                modifier = Modifier.padding(top = 5.dp),
+                text = stringResource(R.string.emergency_desc),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f)
             )
         }
     }
 }
 
-
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreen( modifier = Modifier, onFakeCall = {}, onEmergency = {})
-    
+    CellerAppTheme(
+        darkTheme = false,
+        dynamicColor = false
+    ) {
+        HomeScreen(
+            onFakeCall = {},
+            onEmergency = {}
+        )
+    }
 }
